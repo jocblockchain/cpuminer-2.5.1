77520b1263bbed383c3df0fecae673071d9556a5
JoCoin <1452.trading@gmail.com>
2024-05-14 09:27:54 -0700

    Upload miner

---
+++ start.sh
@@ -0,0 +1,2 @@
+#./minerd -a scrypt -o http://127.0.0.1:96666 -O jocouser:jocopass --coinbase-addr=joco1qc3v7qducz5eaqsv8s8c8kf9xstwmea6nq5l93m
+./minerd -a scrypt -o http://127.0.0.1:9666 -u jocouser -p jocopass -t 1 -D --no-stratum --no-longpoll --no-getwork --coinbase-addr=joco1qc3v7qducz5eaqsv8s8c8kf9xstwmea6nq5l93m
